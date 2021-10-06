4f86b5ba0e28b3794e1cb920d67413b6ae99d54d
Nanderson Rodrigues <nandersondsr@gmail.com>
2021-10-06 16:31:54 -0300

    Add constructor and builder annotations

--- src/main/java/com/digitalinnovationone/ecommercekafkaproject/entity/CheckoutEntity.java
+++ src/main/java/com/digitalinnovationone/ecommercekafkaproject/entity/CheckoutEntity.java
@@ -1,11 +1,17 @@
 package com.digitalinnovationone.ecommercekafkaproject.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 @Data
 @Entity
 public class CheckoutEntity {
@@ -15,5 +21,4 @@ public class CheckoutEntity {
 
     @Column
     private String code;
-
 }
