9b6407357c3b28548b7353201e1629d5c0c3e603
Nanderson Rodrigues <nandersondsr@gmail.com>
2021-10-06 16:35:07 -0300

    Add dependency injection of ChekckoutService class

--- src/main/java/com/digitalinnovationone/ecommercekafkaproject/resource/checkout/CheckoutResource.java
+++ src/main/java/com/digitalinnovationone/ecommercekafkaproject/resource/checkout/CheckoutResource.java
@@ -1,5 +1,6 @@
 package com.digitalinnovationone.ecommercekafkaproject.resource.checkout;
 
+import com.digitalinnovationone.ecommercekafkaproject.service.CheckoutService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,7 +11,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class CheckoutResource {
 
+    private final CheckoutService chekcoutService;
+
     public ResponseEntity<Void> create(CheckoutRequest checkoutRequest) {
+
+        chekcoutService.create(checkoutRequest);
+
         return ResponseEntity.ok().build();
     }
 
