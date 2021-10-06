fd38049dd9e2fc0eeef3ef2d3f4b6af7927605ef
Nanderson Rodrigues <nandersondsr@gmail.com>
2021-10-06 16:46:20 -0300

    Add method to create an Entity

--- src/main/java/com/digitalinnovationone/ecommercekafkaproject/service/CheckoutService.java
+++ src/main/java/com/digitalinnovationone/ecommercekafkaproject/service/CheckoutService.java
@@ -1,4 +1,11 @@
 package com.digitalinnovationone.ecommercekafkaproject.service;
 
+import com.digitalinnovationone.ecommercekafkaproject.entity.CheckoutEntity;
+import com.digitalinnovationone.ecommercekafkaproject.resource.checkout.CheckoutRequest;
+
+import java.util.Optional;
+
 public interface CheckoutService {
+
+    Optional<CheckoutEntity> create(CheckoutRequest checkoutRequest);
 }
--- src/main/java/com/digitalinnovationone/ecommercekafkaproject/service/CheckoutServiceImpl.java
+++ src/main/java/com/digitalinnovationone/ecommercekafkaproject/service/CheckoutServiceImpl.java
@@ -1,7 +1,25 @@
 package com.digitalinnovationone.ecommercekafkaproject.service;
 
+import com.digitalinnovationone.ecommercekafkaproject.entity.CheckoutEntity;
+import com.digitalinnovationone.ecommercekafkaproject.repository.CheckoutRepository;
+import com.digitalinnovationone.ecommercekafkaproject.resource.checkout.CheckoutRequest;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+import java.util.UUID;
+
 @Service
+@RequiredArgsConstructor //constructor for all final fields
 public class CheckoutServiceImpl implements CheckoutService{
+
+    private final CheckoutRepository checkoutRepository;
+
+    @Override
+    public Optional<CheckoutEntity> create(CheckoutRequest checkoutRequest) {
+        final CheckoutEntity checkoutEntity = CheckoutEntity.builder()
+                .code(UUID.randomUUID().toString())
+                .build();
+        return Optional.of(checkoutRepository.save(checkoutEntity));
+    }
 }
